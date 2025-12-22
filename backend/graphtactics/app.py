473ab10b1603472c49dac67f5a35ba779fdfd35a
Guillaume Barreau <g.barreau@gmail.com>
2025-12-22 11:16:09 +0100

    set default port to 8081.

--- backend/graphtactics/app.py
+++ backend/graphtactics/app.py
@@ -169,5 +169,5 @@ async def generate_plan(scenario_dto: ScenarioDTO):
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8081))
     uvicorn.run(app, host="0.0.0.0", port=port)
